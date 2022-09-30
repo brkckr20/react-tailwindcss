a958b2b801b20eb5a9571fc97ff943aab2b7385e
Burak Çakır <burak2043@gmail.com>
2022-09-30 15:02:45 +0300

    pricing eklendi

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import About from "./components/About";
 import Hero from "./components/Hero";
 import Support from "./components/Support";
 import Navbar from "./components/Navbar";
+import AllInOne from "./components/AllInOne";
+import Pricing from "./components/Pricing";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Hero />
       <About />
       <Support />
+      <AllInOne />
+      <Pricing />
     </>
   );
 }
---
+++ src/components/AllInOne.jsx
@@ -0,0 +1,106 @@
+import React from 'react'
+
+const AllInOne = () => {
+    return (
+        <div className='w-full my-32'>
+            <div className='max-w-[1240px] mx-auto px-2'>
+                <h2 className='text-5xl font-bold text-center'>All In One Platform</h2>
+                <p className='text-2xl py-8 text-gray-500 text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia porro neque fugit delectus enim, excepturi hic tempora nisi quisquam placeat.</p>
+                <div className='grid sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4'>
+
+                        <div className='flex'>
+                            <div>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 mr-4 text-green-600">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+                            </svg>
+                            </div>
+                            <div >
+                                <h3 className='font-bold text-lg'>Notifications</h3>
+                                <p className='text-lg pt-2 pb-4'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima explicabo sed cupiditate nobis tenetur accusamus autem maxime veritatis fuga porro.</p>
+                            </div>
+                        </div>
+                        <div className='flex'>
+                            <div>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 mr-4 text-green-600">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+                            </svg>
+                            </div>
+                            <div >
+                                <h3 className='font-bold text-lg'>Notifications</h3>
+                                <p className='text-lg pt-2 pb-4'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima explicabo sed cupiditate nobis tenetur accusamus autem maxime veritatis fuga porro.</p>
+                            </div>
+                        </div>
+                        <div className='flex'>
+                            <div>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 mr-4 text-green-600">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+                            </svg>
+                            </div>
+                            <div >
+                                <h3 className='font-bold text-lg'>Notifications</h3>
+                                <p className='text-lg pt-2 pb-4'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima explicabo sed cupiditate nobis tenetur accusamus autem maxime veritatis fuga porro.</p>
+                            </div>
+                        </div>
+                        <div className='flex'>
+                            <div>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 mr-4 text-green-600">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+                            </svg>
+                            </div>
+                            <div >
+                                <h3 className='font-bold text-lg'>Notifications</h3>
+                                <p className='text-lg pt-2 pb-4'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima explicabo sed cupiditate nobis tenetur accusamus autem maxime veritatis fuga porro.</p>
+                            </div>
+                        </div>
+                        <div className='flex'>
+                            <div>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 mr-4 text-green-600">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+                            </svg>
+                            </div>
+                            <div >
+                                <h3 className='font-bold text-lg'>Notifications</h3>
+                                <p className='text-lg pt-2 pb-4'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima explicabo sed cupiditate nobis tenetur accusamus autem maxime veritatis fuga porro.</p>
+                            </div>
+                        </div>
+                        <div className='flex'>
+                            <div>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 mr-4 text-green-600">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+                            </svg>
+                            </div>
+                            <div >
+                                <h3 className='font-bold text-lg'>Notifications</h3>
+                                <p className='text-lg pt-2 pb-4'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima explicabo sed cupiditate nobis tenetur accusamus autem maxime veritatis fuga porro.</p>
+                            </div>
+                        </div>
+                        <div className='flex'>
+                            <div>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 mr-4 text-green-600">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+                            </svg>
+                            </div>
+                            <div >
+                                <h3 className='font-bold text-lg'>Notifications</h3>
+                                <p className='text-lg pt-2 pb-4'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima explicabo sed cupiditate nobis tenetur accusamus autem maxime veritatis fuga porro.</p>
+                            </div>
+                        </div>
+                        <div className='flex'>
+                            <div>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 mr-4 text-green-600">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+                            </svg>
+                            </div>
+                            <div >
+                                <h3 className='font-bold text-lg'>Notifications</h3>
+                                <p className='text-lg pt-2 pb-4'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima explicabo sed cupiditate nobis tenetur accusamus autem maxime veritatis fuga porro.</p>
+                            </div>
+                        </div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default AllInOne
